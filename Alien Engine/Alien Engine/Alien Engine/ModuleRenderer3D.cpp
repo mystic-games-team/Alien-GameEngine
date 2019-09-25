baa3d980e2386882f67a9a17b1c3da8192bd3a29
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-09-25 12:17:12 +0200

    fix lib

--- Alien Engine/Alien Engine/Alien Engine/ModuleRenderer3D.cpp
+++ Alien Engine/Alien Engine/Alien Engine/ModuleRenderer3D.cpp
@@ -8,7 +8,7 @@
 
 #pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
-#pragma comment (lib, "glew/libx86/glew32.lib") 
+#pragma comment (lib, "glew/glew32.lib") 
 
 
 
