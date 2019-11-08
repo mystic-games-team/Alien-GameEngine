513b63a2daf27ebcea4a0333714b3e0d60ba36b4
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-11-08 22:39:27 +0100

    ups

--- Alien Engine/Alien Engine/GameObject.cpp
+++ Alien Engine/Alien Engine/GameObject.cpp
@@ -476,7 +476,7 @@ void GameObject::SaveObject(JSONArraypack* to_save, const uint& family_number)
 	to_save->SetString("Name", name);
 	to_save->SetNumber("FamilyNumber", family_number);
 	to_save->SetString("ID", std::to_string(ID));
-	to_save->SetString("ParentID",(parent != nullptr) ? std::to_string(parent->ID) : 0);
+	to_save->SetString("ParentID",(parent != nullptr) ? std::to_string(parent->ID) : "0");
 	to_save->SetBoolean("Enabled", enabled);
 	to_save->SetBoolean("ParentEnabled", parent_enabled);
 	to_save->SetBoolean("Selected", selected);
