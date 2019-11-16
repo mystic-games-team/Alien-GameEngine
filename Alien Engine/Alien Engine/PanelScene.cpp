2e26fd25a15c1ca177af7b0b4252dd0211665f05
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2019-11-16 16:01:14 +0100

    Fix

--- Alien Engine/Alien Engine/PanelScene.cpp
+++ Alien Engine/Alien Engine/PanelScene.cpp
@@ -6,6 +6,7 @@
 #include "FileNode.h"
 #include "PanelSceneSelector.h"
 #include "ComponentTransform.h"
+#include "ReturnZ.h"
 
 PanelScene::PanelScene(const std::string& panel_name, const SDL_Scancode& key1_down, const SDL_Scancode& key2_repeat, const SDL_Scancode& key3_repeat_extra)
 	: Panel(panel_name, key1_down, key2_repeat, key3_repeat_extra)
