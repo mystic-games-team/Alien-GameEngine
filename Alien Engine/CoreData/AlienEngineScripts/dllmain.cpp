0f6270f8897579170ad8b3272687c735aa40cb9b
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-12-03 20:19:33 +0100

    fix

--- Alien Engine/CoreData/AlienEngineScripts/dllmain.cpp
+++ Alien Engine/CoreData/AlienEngineScripts/dllmain.cpp
@@ -1,5 +1,5 @@
 // dllmain.cpp : Define el punto de entrada de la aplicación DLL.
-#include "framework.h"
+#include <windows.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
--- Alien Engine/CoreData/AlienEngineScripts/framework.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define WIN32_LEAN_AND_MEAN             // Excluir material rara vez utilizado de encabezados de Windows
-// Archivos de encabezado de Windows
-#include <windows.h>
