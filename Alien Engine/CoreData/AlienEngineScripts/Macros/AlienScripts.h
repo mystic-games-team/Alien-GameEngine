a81ebef4f8b9413fcb97c1e50217dc4bc7c3820e
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-12-04 19:36:19 +0100

    reorder

--- Alien Engine/CoreData/AlienEngineScripts/Macros/AlienScripts.h
+++ Alien Engine/CoreData/AlienEngineScripts/Macros/AlienScripts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "..\..\Alien Engine\ComponentScript.h"
+#include "..\..\..\Alien Engine\ComponentScript.h"
 
 #ifdef ALIENENGINESCRIPTS_EXPORTS
 #define ALIEN_ENGINE_API __declspec(dllexport)
@@ -12,7 +12,6 @@
 #define SHOW_IN_INSPECTOR /*ShowInInspector*/
 
 #define ALIEN_ENGINE_CLASS_API ALIEN_ENGINE_API
-#define ALIEN_ENGINE_ABSTRACT_CLASS_API ALIEN_ENGINE_API
 #define ALIEN_ENGINE_STRUCT_API ALIEN_ENGINE_API
 
 #define ALIEN_INIT_CLASS ALIEN_ENGINE_API
@@ -20,7 +19,6 @@
 
 #define ALIEN_INIT_HEADER extern "C" {
 #define ALIEN_END_HEADER }
-
 
 
 #define SHOW_IN_INSPECTOR_AS_INPUT_INT(INT_) ComponentScript::InspectorInputInt(&INT_);
